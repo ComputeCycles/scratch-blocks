30ccd724963d2a98800cd976bd0106ad7c10e3cf
Benjamin Schweizer <bschweiz@gmail.com>
2021-05-21 10:20:03 -0500

    removed Game Control blocks, moved mqtt sound blocks

--- blocks_vertical/default_toolbox.js
+++ blocks_vertical/default_toolbox.js
@@ -545,33 +545,6 @@ Blockly.Blocks.defaultToolbox = '<xml id="toolbox-categories" style="display: no
       '</value>' +
     '</block>' +
   '</category>' +
-  '<category name="Game Control" id="countdownGame" colour="#CF173B" secondaryColour="#900C3F" ' +
-  'showStatusButton="false">' +
-    '<block type="countdown_gameMode" id="countdown_gameMode"></block>' +
-    '<block type="countdown_gameModeCheck" id="countdown_gameModeCheck"></block>' +
-  '</category>' +
-  '<category name="Lights" id="lights" colour="#3399ff" secondaryColour="#3399ff" ' +
-  'showStatusButton="false">' +
-    '<block type="lights_startsequence">' +
-      '<value name="VALUE">' +
-        '<shadow type="text">' +
-          '<field name="TEXT">File Name</field>' +
-        '</shadow>' +
-      '</value>' +
-    '</block>' +
-    '<block type="lights_sendMessage">' +
-      '<value name="VALUE">' +
-        '<shadow type="text">' +
-          '<field name="TEXT">sequence</field>' +
-        '</shadow>' +
-      '</value>' +
-      '<value name="SATELLITE">' +
-        '<shadow type="text">' +
-          '<field name="TEXT">Satellite #</field>' +
-        '</shadow>' +
-      '</value>' +
-    '</block>' +
-  '</category>' +
   '<category name="Messages" id="messages" colour="#008080" secondaryColour="#086363" ' +
     'showStatusButton="false">' +
       '<block type="message_sendGameMQTT">' +
